8e3bb30e1ef2695ccbce85e66a59195378576d09
Ikotun Collins <46540314+collinscodes2005@users.noreply.github.com>
2022-09-15 18:35:12 +0100

    Update info.py

--- chiton/info.py
+++ chiton/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'myapp.chiton.django@gmail.com'
-EMAIL_HOST_PASSWORD = 'kuuvtnowjdazprxz'
+EMAIL_HOST_PASSWORD = 'kuuvtnowjdazprxz..'
 EMAIL_PORT = 587
